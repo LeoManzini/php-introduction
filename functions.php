5f83e81599a8a5a4bd8c9c50879fe0518ca83e47
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-04-29 17:03:16 -0300

    feat: functions references

--- functions.php
+++ functions.php
@@ -4,6 +4,17 @@
 // Isso é útil para evitar erros de comparação entre tipos diferentes
 declare(strict_types = 1);
 
+include 'square.php';
+// O require é mais forte que o include, se não encontrar o arquivo, dá erro fatal
+// O include dá erro de aviso, mas continua a execução do código
+// require 'square.php';
+// include_once 'square.php';
+// require_once 'square.php';
+// O include_once e require_once só incluem o arquivo uma vez, mesmo que sejam chamados várias vezes
+// Isso é útil para evitar erros de inclusão de arquivos duplicados
+var_dump(calcularQuadrado(5));
+print '<br>';
+
 // Declaração de variáveis na assinatura da função com tipos de dados
 // Declaração de uma função com tipo de retorno
 // O PHP não tem tipagem forte, mas podemos declarar os tipos de dados que a função deve receber e retornar
---
+++ square.php
@@ -0,0 +1,5 @@
+<?php 
+function calcularQuadrado($lado) {
+    return $lado * $lado;
+}
+?>
