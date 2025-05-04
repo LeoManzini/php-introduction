6671eccf5778452821da245c55f80e63dd437e10
Leonardo H Manzini <leoh.manzini@gmail.com>
2025-05-04 00:56:57 -0300

    feat: PHP is a DDD and have a different approach than Java

--- module-02/classes/Cart.php
+++ module-02/classes/Cart.php
@@ -22,6 +22,21 @@ class Cart {
         return $this->products;
     }
 
+    /**
+     * Métodos auxiliares como esse manter na entidade. Em PHP (e em algumas abordagens mais orientadas a objetos como DDD — Domain-Driven Design),
+     * é perfeitamente aceitável e até recomendado colocar esse tipo de lógica de domínio dentro da própria entidade.
+     * 
+     * Por que getTotalPrice() no Cart faz sentido em PHP (e DDD)?
+     *     Coesão: O cálculo do total faz parte do conceito de um carrinho. Não faria sentido ele existir sem essa capacidade.
+     *     Encapsulamento: A classe Cart já conhece os produtos, então ela é o lugar mais natural para saber como calcular o total.
+     *     Evita services inchados: Separar tudo em Service pode levar a classes que fazem “tudo e nada ao mesmo tempo”.
+     * Pensamento moderno: "Rich Domain Model"
+     *     Em vez de ter entidades burras e services inteligentes, você cria entidades ricas, com comportamentos próprios. Isso torna seu código mais modular, testável e legível.
+     *  Quando usar um CartService, então?
+     *     Quando você precisa de operações complexas envolvendo múltiplos agregados (ex: Cart, Inventory, Discounts, Coupons, Shipping...).
+     *     Quando há regras que envolvem acesso a repositórios, ou integração externa.
+     *     Quando a lógica não pertence a um único objeto, mas a uma orquestração entre vários.
+     */
     public function getTotalPrice(): float {
         $total = 0.0;
         foreach ($this->products as $product) {
